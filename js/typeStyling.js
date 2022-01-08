3df4071492b253c823a2253aadd71c12e3076ff7
carsakiller <61925890+carsakiller@users.noreply.github.com>
2022-01-07 23:52:57 -0500

    page generation gets descriptions from inputs

--- js/typeStyling.js
+++ js/typeStyling.js
@@ -8,50 +8,40 @@ for (let e of dataTypes) {
 
     switch(type) {
         case "steamid":
-            color = '#f6496f';
             e.setAttribute('data-tippy-content', 'A steam_id is a 17 digit number unique to a certain player. It is used to uniquely identify players within C².');
             break;
         case "peerid":
-            color = '#01b771';
-            e.setAttribute('data-tippy-content', 'The peer_id of a user is a unique number given to players by Stormworks. These ids can be seen by using the tilde (~) key in-game.');
+            e.setAttribute('data-tippy-content', 'The peerID of a user is a unique number given to players by Stormworks. These ids can be seen by using the tilde (~) key in-game.');
             break;
         case "playerid":
-            color = '#757af4';
-            e.setAttribute('data-tippy-content', 'A playerID is either the player\'s peer_id or name. You can see a player\'s peer_id using the tilde (~) key in game');
+            e.setAttribute('data-tippy-content', 'A playerID is either the player\'s peerID or name. You can see a player\'s peer_id using the tilde (~) key in game');
             break;
         case "vehicleid":
-            color = '#d1ce3e';
             e.setAttribute('data-tippy-content', 'A vehicleID is a unique number given to each vehicle on spawn by Stormworks. There is no way to view these in vanilla Stormworks.');
             break;
         case "itemid":
-            color = '#0099dd';
-            e.setAttribute('data-tippy-content', 'An item_id is a unique number given to each piece of equipment from Stormworks.');
+            e.setAttribute('data-tippy-content', 'An itemID is a unique number given to each piece of equipment from Stormworks.');
             break;
         case "location":
-            color = '#c25706';
             e.setAttribute('data-tippy-content', 'A named location that C² has added to the game.');
             break;
         case "letter":
-            color = '#ffea57';
             e.setAttribute('data-tippy-content', 'A capital letter like "A" or "B".');
             break;
         case "bool":
-            color = '#f92672';
             e.setAttribute('data-tippy-content', 'A boolean value like "true" or "false".');
             break;
         case "number":
-            color = '#ae81ff';
             e.setAttribute('data-tippy-content', 'A integer or float value like "24" or "3.14".');
             break;
         case "string":
-            color = '#e6db74';
             e.setAttribute('data-tippy-content', 'Some text like "hello" or "a".');
             break;
         default:
-            color = 'magenta';
+            console.warn(`Unrecognized type "${type}"\n${e}`);
     }
 
-    e.style.color = color;
+    e.setAttribute('data-type', type)
 }
 
 // tooltips for data types
